185645b3591542746da3a4f36d8e5dd173adc515
Yoonbin Kim <88513592+Wabyx@users.noreply.github.com>
2022-05-02 22:35:42 +0900

    Add files via upload

---
+++ search_engine.py
@@ -0,0 +1,34 @@
+from time import strftime
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
+url = "https://datalab.naver.com/keyword/realtimeList.naver?age=20s"
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text, 'html.parser')
+rank = 1
+# span - item_title
+results = soup.findAll("span", "item_title")
+
+print(response.text)
+# file = open("daum.html","w")
+# file.write(response.text)
+# file.close()
+
+#print(soup.title)
+#print(soup.string)
+#print(soup.span)
+#print(soup.findAll('span'))
+
+
+
+
+search_rank_file = open("rankresult.txt","a")
+
+print(datetime.today().strftime("%y년 %m월 %d일의 실시간 검색어 순위입니다.\n"))
+
+for result in results:
+    search_rank_file.write(str(rank) + "위: " + result.get_text()+"\n")
+    print(results, "위 : ", result.get_text(),"\n")
+    rank += 1 
+
